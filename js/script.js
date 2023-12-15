4dff583af620bf0d9c3fe473db33ca550bfced78
Maximus Aurelius Wiranata <83909501+maxx2002@users.noreply.github.com>
2023-12-15 23:23:42 +0700

    init

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const burger = document.querySelector("#burger");
+const menu = document.querySelector("#menu");
+
+burger.addEventListener("click", () => {
+    if (menu.classList.contains("hidden")) {
+        menu.classList.remove("hidden");
+    } else {
+        menu.classList.add("hidden");
+    }
+});
+
+// window.addEventListener('load', () => {
+//     revealFunction();
+// })
+
+// function revealFunction(){
+//     window.sr = ScrollReveal({ duration:1000, distance:'50px', easing:'ease-out'});
+
+//     sr.reveal('.reveal-left', {origin:'left', reset:false});
+//     sr.reveal('.reveal-top', {origin:'top', reset:false});
+//     sr.reveal('.reveal-bottom', {origin:'bottom', reset:false});
+//     sr.reveal('.reveal-right', {origin:'right', reset:false});
+//     sr.reveal('.reveal-rotate', {origin:'bottom', rotate:{x:1000, z:1000}, reset:false});
+// }
